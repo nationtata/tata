a1c1bb48af78abce4a69cf580bf525bdf916f28a
tata <327203987+nationtata@users.noreply.github.com>
2026-09-14 15:14:11 +0300

    Update addon.js

--- addon.js
+++ addon.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const fs = require("fs");
+const { getNetwork } = require("./tmdb");
 
 const {
   getGroups,
