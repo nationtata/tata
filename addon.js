3e7be285e40538d9ca8ba229ab4a831859109121
tata <327203987+nationtata@users.noreply.github.com>
2026-09-14 21:37:14 +0300

    Update addon.js

--- addon.js
+++ addon.js
@@ -111,7 +111,7 @@ app.get("/catalog/tv/:id.json", (req, res) => {
       id: `tv-${channel.id}`,
       type: "tv",
       name: channel.name,
-      poster: absolute(req, assets.logo),
+      poster: absolute(req, assets.poster),
       logo: absolute(req, assets.logo),
       posterShape: "square"
     };
@@ -142,7 +142,7 @@ app.get("/meta/tv/:id.json", async (req, res) => {
     type: "tv",
     name: channel.name,
     logo: absolute(req, assets.logo),
-    poster: absolute(""),
+    poster: "",
     background: "",
     genres: [channel.group]
   };
